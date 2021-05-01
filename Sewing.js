4d67efd51db6ecd3b738d74040d986294b6e699c
Alex Merkel <69355187+merkel-design@users.noreply.github.com>
2021-05-01 16:05:13 -0500

    tos button additions

--- Sewing.js
+++ Sewing.js
@@ -1,14 +1,21 @@
 const burger = document.getElementById('burger');
-
-
-
+const TOSbutton = document.getElementById('footerTOSbutton');
 
 burger.addEventListener('click', (e) => {
    
-
     hamburgerAnimation();
     
 });
+
+TOSbutton.addEventListener('click', (e) => {
+   
+    tOsExpand();
+    
+});
+
+
+
+
 
 function hamburgerAnimation() {
 
@@ -28,5 +35,28 @@ function hamburgerAnimation() {
     }
 
 
+
+};
+
+function tOsExpand() {
+    const footerGrid = document.getElementById('footer-grid');
+
+    if (footerGrid.classList.value === "default-Grid") {
+        document.getElementById("footerTOSbutton").innerHTML = "Click me!"
+        footerGrid.classList.remove("default-Grid");
+        footerGrid.classList.add("TOS-expanded-grid");
+
+        // changing the text of the bottom button
+
+        
+
+    }
+    else {
+        document.getElementById("footerTOSbutton").innerHTML = "Hello"
+        footerGrid.classList.remove("TOS-expanded-grid");
+        footerGrid.classList.add("default-Grid");
+
+        
+    };
 
 };
