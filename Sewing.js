94ddae9b61342d423e53f3f496c89298ebe9296c
Alex Merkel <69355187+merkel-design@users.noreply.github.com>
2021-05-08 14:12:19 -0500

    created order page

--- Sewing.js
+++ Sewing.js
@@ -90,3 +90,12 @@ function tOsExpand() {
     };
 
 };
+
+// Order page slider JS
+
+var slider = new Slider("#ex13", {
+    ticks: [0, 100, 200, 300, 400],
+    ticks_labels: ['$0', '$100', '$200', '$300', '$400'],
+    ticks_snap_bounds: 30
+});
+Slider();
